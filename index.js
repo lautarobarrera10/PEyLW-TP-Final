f8553eafb01765223b3ebdf67e876c56fe0ddf05
Lautaro Barrera <lautarobarrera12@gmail.com>
2024-06-07 02:08:24 -0300

    empezando a crear el home

--- index.js
+++ index.js
@@ -7,11 +7,23 @@ function login(){
     const usuarioIngresado = document.querySelector('#nombre-usuario').value;
     const password = document.querySelector('#password').value;
 
+    let usuarioValidado = false;
+
     usuarios.forEach(usuario => {
         if (usuario[0] == usuarioIngresado){
             console.log("usuario encontrado");
+            if (usuario[1] == password){
+                console.log("La contraseña coincide");
+                usuarioValidado = true;
+            } else {
+                console.log("La contraseña no coincide")
+            }
         }
     });
+
+    if (usuarioValidado){
+        window.location.href = "home.html";
+    } 
 }
 
 loginButton.addEventListener('click', login);
