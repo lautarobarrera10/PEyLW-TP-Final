a183c3bae2b4ca02e4d385be3cdea5613adc6d0c
lautarobarrera10 <lautarobarrera12@gmail.com>
2024-06-22 12:52:13 -0300

    trabajando en la página de cursos

--- js/login.js
+++ js/login.js
@@ -3,6 +3,24 @@ if (localStorage.getItem("sesion")){
     window.location.href = "./home.html";
 }
 
+// Verificar si el array de usuarios ya está definido en localStorage
+if (!localStorage.getItem("usuarios")) {
+    // Array con usuarios pre-definidos
+    const usuarios = [
+        {
+            nombreUsuario: "admin",
+            password: "admin123",
+            tipo: "administrador"
+        },
+    ];
+
+    // Convertir el array en una cadena JSON
+    const usuariosJSON = JSON.stringify(usuarios);
+
+    // Guardar el array en localStorage
+    localStorage.setItem("usuarios", usuariosJSON);
+}
+
 const loginButton = document.querySelector('#login-button');
 loginButton.addEventListener('click', login);
 
@@ -10,8 +28,12 @@ function login(){
     // Recuperar la cadena JSON desde localStorage
     let usuariosJSON = localStorage.getItem("usuarios");
 
+    console.log(usuariosJSON)
+
     // Convertir la cadena JSON de nuevo en un array
     const usuarios = JSON.parse(usuariosJSON);
+
+    console.log(usuarios)
 
     // Obtenemos los valores ingresados
     const usuarioIngresado = document.querySelector('#nombre-usuario').value;
