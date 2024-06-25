4e3f5c0b980b632f55829d1ced6a411b6e9c0d35
Lautaro Barrera <lautarobarrera12@gmail.com>
2024-06-25 19:30:34 -0300

    trabajando en la validación de la creación de curso

--- js/crear-curso.js
+++ js/crear-curso.js
@@ -15,14 +15,20 @@ const errorNombre = document.querySelector("#nombre-curso-error");
 nombreCursoIngresado.addEventListener("change", validarNombreCurso)
 
 function validarNombreCurso(){
+    let rta;
     if (nombreCursoIngresado.value.length < 8 || nombreCursoIngresado.value.length > 30){
         errorNombre.style.display = "block";
         nombreCursoIngresado.style.border = "1px solid red"
     } else {
+        rta = true;
         errorNombre.style.display = "none";
         nombreCursoIngresado.style.border = "1px solid green"
     }
+    return rta;
 }
+
+// Descripción del curso
+const descripcionCurso = document.querySelector("#descripcion-curso");
 
 // Validar nota de aprobado
 const notaAprobado = document.querySelector("#nota-aprobado");
@@ -31,14 +37,17 @@ const notaAprobadoError = document.querySelector("#nota-aprobado-error");
 notaAprobado.addEventListener("change", validarNotaAprobado);
 
 function validarNotaAprobado(){
+    let rta = false;
     if (Number.isInteger(Number(notaAprobado.value)) &&
-        notaAprobado.value > 5 && notaAprobado.value <= 10) {
+        Number(notaAprobado.value) > 3 && Number(notaAprobado.value) <= 10) {
         notaAprobadoError.style.display = "none";
         notaAprobado.style.border = "1px solid green";
+        rta = true;
     } else {
         notaAprobadoError.style.display = "block";
         notaAprobado.style.border = "1px solid red";
     }
+    return rta;
 }
 
 
@@ -49,11 +58,38 @@ const notaPromocionError = document.querySelector("#nota-promocion-error");
 notaPromocion.addEventListener("change", validarNotaPromocion);
 
 function validarNotaPromocion(){
+    let rta = false;
     if (Number(notaPromocion.value) >= 8 && Number(notaPromocion.value) <= 10) {
         notaPromocionError.style.display = "none";
         notaPromocion.style.border = "1px solid green";
+        rta = true;
     } else {
         notaPromocionError.style.display = "block";
         notaPromocion.style.border = "1px solid red";
+    }
+    return rta;
+}
+
+// Úlima validación al presionar el botón
+const crearCursoButton = document.querySelector("#crear-curso-button");
+
+crearCursoButton.addEventListener("click", validarFormulario);
+
+function validarFormulario(){
+    const nombreValidado = validarNombreCurso()
+    const aprobadoValidado = validarNotaAprobado()
+    const promocionValidada = validarNotaPromocion()
+    if (nombreValidado && aprobadoValidado && promocionValidada){
+        const coleccionCursos = JSON.parse(localStorage.getItem("cursos"));
+        const docente = JSON.parse(localStorage.getItem("sesion"))["nombreUsuario"]
+        const cursoNuevo = {
+                nombre: nombreCursoIngresado.value,
+                descripcion: descripcionCurso.value,
+                notaAprobado: notaAprobado.value,
+                notaPromocion: notaPromocion.value,
+                docente: docente
+        };
+        coleccionCursos.push(cursoNuevo);
+        localStorage.setItem("cursos", JSON.stringify(coleccionCursos));
     }
 }
