d7eb8e0fcd0e88b1ad8ebf9ff76aa592b4e87852
Lautaro Barrera <lautarobarrera12@gmail.com>
2024-06-28 15:55:03 -0300

    botón de agregar o quitar de mis cursos funcionando

--- js/funciones.js
+++ js/funciones.js
@@ -18,6 +18,94 @@ function crearNodoCurso(objCurso){
     img.src = objCurso.img;
     imgContainer.appendChild(img);
 
+
+    // Si no tengo cursos aun
+    if (!localStorage.getItem("mis-cursos")){
+        // Creamos el botón para agregar este curso
+        let tomarCurso = document.createElement("span");
+        tomarCurso.classList.add("material-symbols-outlined");
+        tomarCurso.classList.add("tomarCursoButton");
+        tomarCurso.textContent = "add_circle_outline";
+
+        // Si hacemos click en el botón de agregar, se agrega el primer curso
+        tomarCurso.addEventListener("click", () => {
+            let misCursos = JSON.stringify([objCurso]);
+            localStorage.setItem("mis-cursos", misCursos);
+
+            // Cambiamos el tipo de botón
+            tomarCurso.textContent = "remove_circle_outline";
+            tomarCurso.style.color = "red";
+
+            // Recargamos la página
+            location.reload();
+        });
+
+        // Por último agregamos el nodo al contenedor
+        imgContainer.appendChild(tomarCurso);
+
+    } else {
+        // Si ya tenemos cursos, evaluamos si este ya forma parte
+        let misCursos = JSON.parse(localStorage.getItem("mis-cursos"));
+        let yaEstoyCursando = misCursos.find(curso => curso.nombre == objCurso.nombre);
+
+        // Si ya estoy cursando este curso
+        if (yaEstoyCursando){
+            // Le agregamos el botón de dejar de cursar
+            let tomarCurso = document.createElement("span");
+            tomarCurso.classList.add("material-symbols-outlined");
+            tomarCurso.classList.add("tomarCursoButton");
+            tomarCurso.style.color = "red";
+            tomarCurso.textContent = "remove_circle_outline";
+
+            // Si hacemos click en el botón de quitar
+            tomarCurso.addEventListener("click", () => {
+                // Quitamos el curso del local storage
+                index = misCursos.indexOf(yaEstoyCursando);
+                misCursos.splice(index, 1);
+
+                // Actualizamos el local storage
+                localStorage.setItem("mis-cursos", JSON.stringify(misCursos));
+
+                // Actualizamos el boton
+                tomarCurso.style.color = "green";
+                tomarCurso.textContent = "add_circle_outline";
+
+                // Recargamos la página
+                location.reload();
+            });
+
+            // Por último agregamos el nodo al contenedor
+            imgContainer.appendChild(tomarCurso);
+        } else {
+            // Si ya existe mis cursos pero este no forma parte le agregamos el botón para sumarlo
+            // Creamos el botón para agregar este curso
+            let tomarCurso = document.createElement("span");
+            tomarCurso.classList.add("material-symbols-outlined");
+            tomarCurso.classList.add("tomarCursoButton");
+            tomarCurso.textContent = "add_circle_outline";
+
+            // Si hacemos click en el botón de agregar, se agrega al array de cursos
+            tomarCurso.addEventListener("click", () => {
+                let misCursos = JSON.parse(localStorage.getItem("mis-cursos"));
+                misCursos.push(objCurso);
+
+                // Actualizamos el local storage
+                localStorage.setItem("mis-cursos", JSON.stringify(misCursos));
+
+                // Cambiamos el tipo de botón
+                tomarCurso.textContent = "remove_circle_outline";
+                tomarCurso.style.color = "red";
+
+                // Recargamos la página
+                location.reload();
+            });
+
+            // Por último agregamos el nodo al contenedor
+            imgContainer.appendChild(tomarCurso);
+
+        }
+    }
+
     const nombre = document.createElement("h3");
     nombre.textContent = objCurso.nombre;
     container.appendChild(nombre);
